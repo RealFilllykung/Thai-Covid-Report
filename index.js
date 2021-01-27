b16e6ecd7813740b2c56834c4ad167889916bebd
RealFilllykung <filllykunggm@gmail.com>
2021-01-27 20:09:54 +0700

    Added getTodayHospitalized() feature

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const getTotalConfirmed = require('./functions/totalReport/getTotalConfirmedCase
 const getTodayRecovered = require('./functions/todayReport/getTodayRecovered')
 const getTodayUpdateDate = require('./functions/todayReport/getTodayUpdateDate')
 const getTodayDeath = require('./functions/todayReport/getTodayDeath')
+const getTodayHospitalized = require('./functions/todayReport/getTodayHospitalized')
 
 module.exports = {
     getTodayInfected: () => getTodayInfected(),
     getTotalConfirmed: () => getTotalConfirmed(),
     getTodayRecovered: () => getTodayRecovered(),
     getTodayUpdateDate: () => getTodayUpdateDate(),
-    getTodayDeath: () => getTodayDeath()
+    getTodayDeath: () => getTodayDeath(),
+    getTodayHospitalized: () => getTodayHospitalized()
 }
